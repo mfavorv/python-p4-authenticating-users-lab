11f9b901b3ef8cc7047ddc591fde08ae881b10f8
Favor Vincent <vfavorm77@gmail.com>
2024-07-06 22:29:12 +0300

    authenticating users lab complete

--- server/app.py
+++ server/app.py
@@ -18,10 +18,34 @@ db.init_app(app)
 
 api = Api(app)
 
+class Login(Resource):
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter(User.username == username).first()
+        if user:
+            session['user_id'] = user.id
+            return make_response(user.to_dict(), 200)
+        return {'message': 'User not found'}, 404
+
+class Logout(Resource):
+    def delete(self):
+        session.clear()
+        session['user_id'] = None
+        return make_response({"message": "User logged out."}, 204)
+    
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if not user_id:
+            return make_response({}, 401)         
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+        return make_response({}, 404)  
+    
 class ClearSession(Resource):
 
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
 
@@ -48,6 +72,9 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
